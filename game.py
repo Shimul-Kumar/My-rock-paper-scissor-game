a226a3db22282bda3ec2741504db467f534d8018
Shimul Kumar Shoishob <152776060+Shimul-Kumar@users.noreply.github.com>
2025-06-01 19:22:05 +0000

    complete

---
+++ game.py
@@ -0,0 +1,44 @@
+import random
+rock = '''
+                                  88         
+                                  88         
+                                  88         
+8b,dPPYba,  ,adPPYba,   ,adPPYba, 88   ,d8   
+88P'   "Y8 a8"     "8a a8"     "" 88 ,a8"    
+88         8b       d8 8b         8888[      
+88         "8a,   ,a8" "8a,   ,aa 88`"Yba,   
+88          `"YbbdP"'   `"Ybbd8"' 88   `Y8a  
+                                             
+'''
+
+paper = '''
+8b,dPPYba,  ,adPPYYba, 8b,dPPYba,   ,adPPYba, 8b,dPPYba,  
+88P'    "8a ""     `Y8 88P'    "8a a8P_____88 88P'   "Y8  
+88       d8 ,adPPPPP88 88       d8 8PP""""""" 88          
+88b,   ,a8" 88,    ,88 88b,   ,a8" "8b,   ,aa 88          
+88`YbbdP"'  `"8bbdP"Y8 88`YbbdP"'   `"Ybbd8"' 88          
+88                     88                                 
+88                     88                 
+'''
+scissors = '''
+                                                           
+.d8888b  .d8888b888.d8888b .d8888b  .d88b. 888d888.d8888b  
+88K     d88P"   88888K     88K     d88""88b888P"  88K      
+"Y8888b.888     888"Y8888b."Y8888b.888  888888    "Y8888b. 
+     X88Y88b.   888     X88     X88Y88..88P888         X88 
+ 88888P' "Y8888P888 88888P' 88888P' "Y88P" 888     88888P' 
+                                                          
+'''
+game = [rock, paper, scissors]
+user_input = int(input("Enter rock(1), paper(2), or scissors(3): "))
+print("user :")
+print(game[user_input-1])
+computer = random.randint(1, 3)
+print("computer :")
+print(game[computer-1])
+if user_input == computer:
+    print("Draw")
+elif user_input == (computer%3)+1:
+    print("You win")
+elif user_input == (computer%3)-1:
+    print("You lose")
